81fe75c368ef8448d64e4a5ca8d402914cffbfd6
Huseyin Ozcan <hsynozcn009@gmail.com>
2023-09-22 01:06:34 +0300

    Web Api katmanı eklendi

---
+++ WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,53 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        IProductService _productService;
+        // IoC İnversion of Control   
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        [HttpGet("getall")]
+        public IActionResult  GetAll()
+        {
+            var result = _productService.GetAll();
+
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return  BadRequest(result.Message);
+        }
+        [HttpPost("add")]
+        public IActionResult Post(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest();
+        }
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+    }
+}
